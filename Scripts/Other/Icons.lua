900b10478d9734fa0ee939bbce8aad3dddb9a70e
EgorexW <olko.wiktor.10@gmail.com>
2025-11-14 11:53:28 +0100

    Double enriching recipes

--- Scripts/Other/Icons.lua
+++ Scripts/Other/Icons.lua
@@ -11,6 +11,30 @@ function RichIcons(icon)
     }
 end
 
+function CombinedRichIcon(icon1raw, icon2raw)
+    icon1 = {
+        icon = icon1raw,
+        scale = 0.5,
+        shift = {-4, 4}
+    }
+    icon2 = {
+        icon = icon2raw,
+        scale = 0.5,
+        shift = {4, -4}
+    }
+    return{
+            icon1,
+            icon2,
+        {
+            icon = "__Igrys__/Assets/Other/rich-icon.png",
+            icon_size = 16,
+            scale = 1,
+            shift = {8, 8},
+            --tint = {r = 0, g = 0, b = 0, a = 0.5}
+        },
+    }
+end
+
 function MadeFromIcons(iconFile, madeFromIconFile)
     icon = {icon = iconFile}
     madeFromIcon = {icon = madeFromIconFile}
--- Scripts/Recipes/EnrichingMagicParts.lua
+++ Scripts/Recipes/EnrichingMagicParts.lua
@@ -3,13 +3,16 @@ data:extend({
         type = "recipe",
         name = "igrys-rich-magic-part-1",
         enabled = settings.startup["igrys-enable-all"].value,
+        icons = CombinedRichIcon(data.raw["item"]["igrys-magic-part-1"].icon, data.raw["item"]["igrys-magic-part-2"].icon),
         allow_productivity = true,
         ingredients = {
             {type="item", name="igrys-magic-part-1", amount=1},
+            {type="item", name="igrys-magic-part-2", amount=1},
             {type="item", name="igrys-glass", amount=10}
         },
         results = {
             {type="item", name="igrys-rich-magic-part-1", amount=1},
+            {type="item", name="igrys-rich-magic-part-2", amount=1},
         },
         energy_required = 5,
         category = "advanced-crafting",
@@ -17,13 +20,16 @@ data:extend({
         type = "recipe",
         name = "igrys-rich-magic-part-2",
         enabled = settings.startup["igrys-enable-all"].value,
+        icons = CombinedRichIcon(data.raw["item"]["igrys-magic-part-2"].icon, data.raw["item"]["igrys-magic-part-3"].icon),
         allow_productivity = true,
         ingredients = {
             {type="item", name="igrys-magic-part-2", amount=1},
+            {type="item", name="igrys-magic-part-3", amount=1},
             {type="item", name="igrys-conductive-brick", amount=10}
         },
         results = {
             {type="item", name="igrys-rich-magic-part-2", amount=1},
+            {type="item", name="igrys-rich-magic-part-3", amount=1},
         },
         energy_required = 5,
         category = "advanced-crafting",
@@ -31,13 +37,16 @@ data:extend({
         type = "recipe",
         name = "igrys-rich-magic-part-3",
         enabled = settings.startup["igrys-enable-all"].value,
+        icons = CombinedRichIcon(data.raw["item"]["igrys-magic-part-3"].icon, data.raw["item"]["igrys-magic-part-1"].icon),
         allow_productivity = true,
         ingredients = {
             {type="item", name="igrys-magic-part-3", amount=1},
+            {type="item", name="igrys-magic-part-1", amount=1},
             {type="item", name="igrys-cermite", amount=1}
         },
         results = {
             {type="item", name="igrys-rich-magic-part-3", amount=1},
+            {type="item", name="igrys-rich-magic-part-1", amount=1},
         },
         energy_required = 5,
         category = "advanced-crafting",
