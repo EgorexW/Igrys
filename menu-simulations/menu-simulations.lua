0b181e1a3b468a81c1cd615e124293bd04fed828
EgorexW <olko.wiktor.10@gmail.com>
2025-12-20 10:31:30 +0100

    1.5.17

--- menu-simulations/menu-simulations.lua
+++ menu-simulations/menu-simulations.lua
@@ -121,3 +121,14 @@ menu_simulations["igrys-sim-6"] = -- Simulation name
       game.players[1].game_view_settings.show_controller_gui = false
     ]],
 }
+
+--data.raw["utility-constants"]["default"].main_menu_simulations = {}
+
+t = data.raw["utility-constants"]["default"].main_menu_simulations
+
+t.IgrysSim1 = menu_simulations["igrys-sim-1"]
+t.IgrysSim2 = menu_simulations["igrys-sim-2"]
+t.IgrysSim3 = menu_simulations["igrys-sim-3"]
+t.IgrysSim4 = menu_simulations["igrys-sim-4"]
+t.IgrysSim5 = menu_simulations["igrys-sim-5"]
+t.IgrysSim6 = menu_simulations["igrys-sim-6"]
