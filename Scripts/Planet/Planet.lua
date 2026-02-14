fb451b629c0544ebcb1b8f530563b4d3f4ba1f02
EgorexW <olko.wiktor.10@gmail.com>
2026-02-14 21:21:29 +0100

    1.5.30

--- Scripts/Planet/Planet.lua
+++ Scripts/Planet/Planet.lua
@@ -39,7 +39,7 @@ PlanetsLib:extend({
             ["solar-power"] = 600, -- No atmosphere
             pressure = 3000,
             gravity = 3,
-            ["magic"] = 1,
+            ["igrys-magic"] = 1,
         },
         asteroid_spawn_influence = 1,
         asteroid_spawn_definitions = asteroid_util.spawn_definitions(asteroid_util.gleba_fulgora, 0.9),
@@ -79,7 +79,7 @@ data:extend{PlanetsLib.visit_planet_achievement(data.raw["planet"]["igrys"], "__
 data:extend({
     {
         type = "surface-property",
-        name = "magic",
+        name = "igrys-magic",
         default_value = 0,
         hidden_in_factoriopedia = true,
         hidden = true,
